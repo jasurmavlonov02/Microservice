11a50763da777c3548b229187adb11e7b5f9daad
jasurmavlonov02 <jasurmavlonov24@gmail.com>
2023-05-10 15:13:28 +0500

    ..

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -203,7 +204,7 @@ BATON = {
 
     )
 }
-
+#
 INTERNAL_IPS = [
     # ...
     "127.0.0.1",
