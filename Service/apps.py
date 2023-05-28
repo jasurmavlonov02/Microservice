4833ab5cb7d29049804a384113064379d3933ae9
jasurmavlonov02 <jasurmavlonov24@gmail.com>
2023-05-28 18:11:15 +0500

    apps

--- Service/apps.py
+++ Service/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Service'
-    verbose_name = ''
+    verbose_name = 'Апп'
